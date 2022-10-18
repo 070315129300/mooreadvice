a8b39903454724b9844635ed73ad7ed2714ac829
k-leb <ikhidecaleb@gmail.com>
2022-10-18 16:29:31 +0100

    mooreadvice

---
+++ README.md
@@ -0,0 +1,19 @@
+Name of project => Mooreadvice
+
+This project is a simple registration and login project
+
+The project uses santum for authentication
+
+How to setup =>
+
+Pull project from github
+
+set up your xampp
+
+create a database on phpmyadmin 
+
+name the database mooreadvice
+
+run php artisan migrate
+
+test api with postman
---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Http\Controllers;
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return User::all();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+       return User::find($id);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function update(Request $request, $id)
+    {
+        $product = User::find($id);
+
+        $product->update($request->all());
+        return $product;
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        //
+        return User::destroy($id);
+
+
+    }
+}
